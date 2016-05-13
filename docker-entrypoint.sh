d9d63d4d132aef7787bbf898cf963147701cba60
Andrew Shaw Care <andrewshawcare@users.noreply.github.com>
2016-05-13 10:16:38 -0500

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,8 +7,9 @@ mkdir -p $output_directory
 
 while read line; do
   graph_filename=$(echo $line | cut -d' ' -f3)
+  output_filename=${graph_filename%.*}.$output_extension
   dot \
     -T$output_extension \
     $graph_directory/$graph_filename \
-    -o $output_directory/${graph_filename%.*}.$output_extension
+    -o $output_directory/$output_filename
 done < <(inotifywait --monitor --recursive --event modify $graph_directory)
